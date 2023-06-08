b52d0dde9779b02b7ed54b3398a8c0b92a4bd4b2
Šimon Červenka <cervesim@fel.cvut.cz>
2023-06-08 18:36:20 +0200

    5 unit testu, z toho jeden parametrizovanej

--- Arimaa/src/test/java/unitTests/gameEndedTest.java
+++ Arimaa/src/test/java/unitTests/gameEndedTest.java
@@ -22,10 +22,10 @@ public class gameEndedTest {
 
     @ParameterizedTest
     @CsvSource({
-            "'Hf2,De2,Cd2,Mc2,hc7,dd7,me7,rf7, Rf8', true, g",
+            "'Hf2,De2,Cd2,Mc2,hc7,dd7,me7,rf7,Rf8', true, g",
             "'Hd2,De2,Cc2,Rf2,Eh1,hb7,dc7,ed7,rf7', false, null",
             "'Hd3,De4,Cc5,Rf1,Eh6,hb1,dc2,ed5,rf1', true, s",
-            "'Hf2,De2,Cd2,Mc2,hc7,dd7,me7,rf7', true, g"
+            "'Hf2,De2,Cd2,Mc2,hc7,dd7,me7,rf7', true, s"
     })
     public void testGameEndedOnRandomBoard(String boardSetup, boolean gameEnded, String hasWon) {
         ArrayList<String> piecesToSetList = new ArrayList<>(Arrays.asList(boardSetup.split(",")));
@@ -37,7 +37,10 @@ public class gameEndedTest {
 
         Board board = Board.createBoardUsingArray(piecesToSetArray);
         assertEquals(gameEnded, board.gameEnded);
-        assertEquals(hasWon, board.hasWon().toString());
+        if (!hasWon.equals("null")){
+            assertEquals(hasWon, board.hasWon().toString());
+        } else assertNull(board.hasWon());
+
     }
 
 }
